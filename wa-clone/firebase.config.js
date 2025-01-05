ebedced9be5cff5c80d58713b48200ff9433eb61
nyc <neerajyamaji@gmail.com>
2025-01-05 20:35:09 +0530

    wa-clone setup complete

---
+++ wa-clone/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyD6AI8SVuKcrkzc9hD-9vTIwfYaYtDJIWE",
+	authDomain: "wa-clone-nyc.firebaseapp.com",
+	projectId: "wa-clone-nyc",
+	storageBucket: "wa-clone-nyc.firebasestorage.app",
+	messagingSenderId: "665490824994",
+	appId: "1:665490824994:web:62f0a85ed4c05d4e75cf7b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+ 
---
+++ wa-clone/src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./Components/Login.jsx";
+import Chat from "./Components/Chat.jsx";
+import Home from "./Components/Home.jsx";
+import PageNotFound from "./Components/PageNotFound.jsx";
+import Profile from "./Components/Profile.jsx";
+
+function App() {
+	return (
+		<>
+			<h1>WhatsApp Clone</h1>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/login" element={<Login />}></Route>
+				<Route path="/profile" element={<Profile />}></Route>
+				<Route path="/chat/:uniqueID" element={<Chat />}></Route>
+				{/** :uniqueID will match everything that looks gibberish */}
+				<Route path="*" element={<PageNotFound />}></Route>
+				{/** use * for everything else than above */}
+			</Routes>
+		</>
+	);
+}
+
+export default App;
